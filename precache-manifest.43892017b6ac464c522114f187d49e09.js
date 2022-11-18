b2a8703e3186126011f5ae5285fc57a4c58ef829
sirawittoolpeng <sirawittoolpeng@users.noreply.github.com>
2022-11-18 08:27:49 +0000

    deploy: bbdfd99acbbafe107611407393e0c344775a5055

---
+++ precache-manifest.43892017b6ac464c522114f187d49e09.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a0e2496de923e40e0954f809bc6440e",
+    "url": "https://sirawittoolpeng.github.io/task-management-frontend/index.html"
+  },
+  {
+    "revision": "086b02fc5453e62b562d",
+    "url": "https://sirawittoolpeng.github.io/task-management-frontend/static/css/main.5ac26d30.chunk.css"
+  },
+  {
+    "revision": "15ed119da5a9896234b3",
+    "url": "https://sirawittoolpeng.github.io/task-management-frontend/static/js/2.4139d8c4.chunk.js"
+  },
+  {
+    "revision": "086b02fc5453e62b562d",
+    "url": "https://sirawittoolpeng.github.io/task-management-frontend/static/js/main.8ef5fb8a.chunk.js"
+  },
+  {
+    "revision": "c714c7f05ba24b22e962",
+    "url": "https://sirawittoolpeng.github.io/task-management-frontend/static/js/runtime~main.6ded5db9.js"
+  }
+]);
